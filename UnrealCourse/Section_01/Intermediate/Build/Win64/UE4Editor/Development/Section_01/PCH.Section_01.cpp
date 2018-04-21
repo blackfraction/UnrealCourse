10c687f6497af4c4f5139910d7db41a94276a76a
Carlo <carlo.eigenmann@gmail.com>
2018-04-21 15:51:08 +0200

    first commit

---
+++ UnrealCourse/Section_01/Intermediate/Build/Win64/UE4Editor/Development/Section_01/PCH.Section_01.cpp
@@ -0,0 +1,1 @@
+#include "C:/Repo/UnrealCourse/Section_01/Intermediate/Build/Win64/UE4Editor/Development/Section_01/PCH.Section_01.h"
---
+++ UnrealCourse/Section_02/BullCowGame/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void PrintIntro();
+void PlayGame();
+string GetGuess();
+bool AskToPlayAgain();
+
+// application entry proint
+int main()
+{
+	PrintIntro();
+	PlayGame();
+	AskToPlayAgain();
+	// exit application
+	return 0;
+}
+
+
+void PrintIntro()
+{
+	// define word length and print out introduction
+	constexpr int WORD_LENGTH = 9;
+	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?" << endl;
+	cout << endl;
+	return;
+}
+
+
+void PlayGame()
+{
+	// loop for the number of turns asking for guesses
+	constexpr int TURN_COUNT = 5;
+	for (int count = 0; count < TURN_COUNT; count++)
+	{
+		string Guess = GetGuess();
+		// print guess back
+		cout << "Your guess was: " << Guess << endl;
+		cout << endl;
+	}
+}
+
+
+string GetGuess()
+{
+	// ask for players guess
+	cout << "Please take a guess: ";
+	string Guess = "";
+	getline(cin, Guess);
+	return Guess;
+}
+
+bool AskToPlayAgain()
+{
+	cout << "Do you want to play again? (y/n) ";
+	string Response = "";
+	getline(cin, Response);
+	return (Response[0] == 'y') || (Response[0] == 'Y');
+}
